aaa1e00af11cde983db5cfab214aed7e113b1722
JessicaGarson <jessica.garson@gmail.com>
2017-09-24 14:25:55 -0400

    syllabus

---
+++ class1/basics/strings.py
@@ -0,0 +1,10 @@
+a_string = 'hello world!'
+long_string = '''It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way - in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.
+'''
+long_string_with_new_line = '''It was the best of times, it was the worst of times, it was \n the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way - in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.
+'''
+number_for_breakfast = 3
+
+# We'll be discussing string methods 
+
+donuts = 'I ate {0} donuts for breakfast'.format(number_for_breakfast)
---
+++ class1/basics/variables.py
@@ -0,0 +1,17 @@
+jess = 'hello'
+print jess
+sam = 'cool'
+print sam
+age = 32
+print age
+age = 38
+donuts = 'yummy4u'
+print donuts
+student1, student2, student3 = 'jess', 'tim', 'stacy'
+print student1
+print student2
+print student3
+student1 = student2 = student3 = 'julia'
+print student1
+print student2
+print student3
