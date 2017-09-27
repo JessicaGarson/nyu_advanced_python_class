56f12ad6f06bc8f8dacd7e372954d94551a6b4f1
Sarah Black <sarah@172-17-78-206.DYNAPOOL.NYU.EDU>
2017-09-27 18:55:47 -0400

    adding my datatypes file

---
+++ class2/datatypes/sarah_datatypes.py
@@ -0,0 +1,11 @@
+string = 'this is a string datatype '
+print(string)
+print(str(type(string)))
+
+integer = 7
+print(integer)
+print(str(type(integer)))
+
+floatnum = 238.474
+print(floatnum)
+print(str(type(floatnum)))
