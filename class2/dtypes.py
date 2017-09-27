3efbec1514cc5aba0312b89fb71796be7a9edc96
Alice Topping <alice.r.topping@gmail.com>
2017-09-27 19:21:36 -0400

    datatypes

---
+++ class2/dtypes.py
@@ -0,0 +1,22 @@
+# datatypes assignment
+clinic= 'IP'
+clinics = ['IP','UES','NC1', 'NC2'] 
+num_clinics= len(clinics)
+
+clinic1 = clinics[0]
+print (clinic1)
+print(type(clinic1))
+
+clinic_id = 1060
+patient_id = 1234
+float_patient = float('1234')
+
+print(float_patient)
+print (type(float_patient))
+print (clinic)
+print (type(clinic))
+print (num_clinics)
+print(type(num_clinics))
+
+
+
