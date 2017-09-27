e23c618d13fee35e2647c4e64a2f678c0d36cf0d
Dean Goldman <deanjgoldman@gmail.com>
2017-09-27 19:01:25 -0400

    datatypes

---
+++ class2/datatypes/dean_datatypes.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+mystring = "noodle"
+myint = 4
+myfloat = 9.9999
+
+
+print("3 Datatypes in Python\n")
+print("String: {}".format(mystring))
+print("Integer: {}".format(myint))
+print("Float: {}".format(myfloat))
